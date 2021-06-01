d64d7fff68876a14cf2d31e8d0f6560cd1cfb460
AnnaParulava <a554156554156@gmail.com>
2021-06-01 13:51:41 +0300

    Добавлен вывод ответа

--- prim_form.py
+++ prim_form.py
@@ -17,12 +17,22 @@ def prim_form():
 @post('/Primm', method='post')
 def prim_form():
     
-    x = np.array([5, 15, 25, 35, 45, 55])
-    y = np.array([5, 20, 14, 32, 22, 38])
+    rows=int(request.forms.get('num'))
+    x=[]
+    y=[]
+    #если ячейка пустая, то 0
+    for i in range(rows):
+        x.append(int(request.forms.get('fieldX' +str(i))))
+        y.append(int(request.forms.get('fieldY' + str(i))))
+    print(x)
+    print(y)
+
+    #x = np.array([5, 15, 25, 35, 45, 55])
+    #y = np.array([5, 20, 14, 32, 22, 38])
     r, p = scipy.stats.pearsonr(x, y)
     print("corr:", r)
 
-    x = np.array([5, 15, 25, 35, 45, 55]).reshape((-1, 1))
+    x = np.array(x).reshape((-1, 1))
     model = LinearRegression().fit(x, y)
     r_sq = model.score(x, y)
     print('coefficient of determination:', r_sq)
@@ -30,5 +40,10 @@ def prim_form():
     with open('mas_weight.txt', 'w') as file:
         json.dump(r_sq, file)
     file.close()
-    return json.dumps(r_sq)
-
+    return json.dumps(r,r_sq)
+#
+#
+#Ппивет, давно хотел сказать, что ты волчица, а я волк 
+#Ну и того 
+#Ты пояла
+#Alex Wolf
--- views/make_weight.tpl
+++ views/make_weight.tpl
@@ -7,14 +7,11 @@
     </tr>
     %for i in range(rows):
     <tr> <td>node {{i+1}}</td>
-    %for j in range(rows):
-    %if i<j:
-    <td><input type="Number" name="field{{i}}{{j}}" min="0" max="99" placeholder="0"></td>
-    %else:
-    <td><input type="Number" name="field{{i}}{{j}}" min="0" max="99" style="background-color: #606060" readonly></td>
     %end
+    %for j in range(rows):
+    <td><input type="Number" name="field{{j}}" min="0" max="99" placeholder="0"></td>
+    <td><input type="Number" name="field{{j}}" min="0" max="99" style="background-color: #606060" readonly></td>
     %end
     </tr>
-    %end
     </table>
 <br>
